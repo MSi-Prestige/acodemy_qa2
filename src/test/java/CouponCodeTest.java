e0b0a81720f0ae0b18638a1dba1a19ba672b9c5d
Nikita Milka <nikita@milka.me>
2021-10-19 20:35:05 +0300

    [main]: Refactor tests

---
+++ src/test/java/CouponCodeTest.java
@@ -0,0 +1,32 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import page_object.MainPage;
+import utils.WebDriverManager;
+
+import static utils.PropertiesReader.getProperties;
+
+@Slf4j
+public class CouponCodeTest {
+
+    private final WebDriver driver = WebDriverManager.getInstance();
+    private final MainPage mainPage = new MainPage();
+
+    @BeforeEach
+    public void setUp() {
+        driver.get(getProperties().getProperty("home.page"));
+    }
+
+    @Test
+    public void applyCouponTest() {
+    mainPage.selectProductFromListByName("Polo")
+            .addProductToCart();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
